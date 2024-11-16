923f248ca554b8cab5e83aefc6e852c70d733cf1
andrastoth021 <andrastoth021@gmail.com>
2024-11-16 18:54:51 +0100

    implement OwnerService's create method

--- src/service/owner.service.ts
+++ src/service/owner.service.ts
@@ -1,3 +1,5 @@
+import { OwnerToCreate } from "../entity/owner.type";
+import { PetToCreate } from "../entity/pet.type";
 import { OwnerRepository } from "../repository/owner.repository";
 
 export class OwnerService {
@@ -9,5 +11,9 @@ export class OwnerService {
 
   async getAll() {
     return await this.repository.read();
+  }
+  
+  async create(owner: OwnerToCreate) {
+    return await this.repository.create(owner);
   }
 }
