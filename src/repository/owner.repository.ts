5b9599a43eda80be641c4516409244da65e7fc91
andrastoth021 <andrastoth021@gmail.com>
2024-11-16 18:54:12 +0100

    implement OwnerRepository's create method

--- src/repository/owner.repository.ts
+++ src/repository/owner.repository.ts
@@ -1,5 +1,5 @@
 import { DbClient } from "../db";
-import { Owner } from "../entity/owner.type";
+import { Owner, OwnerToCreate } from "../entity/owner.type";
 
 export class OwnerRepository {
   private readonly client;
@@ -12,6 +12,17 @@ export class OwnerRepository {
     const sql = `SELECT id, name, age FROM pet_owner LIMIT $1 OFFSET $2`;
     const rows = await this.client.query(sql, [limit, offset]) as Array<Owner>;
     return rows;
+  }
+
+  async create(owner: OwnerToCreate) {
+    const { name, age } = owner;
+    const sql = `
+      INSERT INTO pet_owner (name, age) 
+      VALUES ($1, $2) 
+      RETURNING * 
+    `;
+    const rows = await this.client.query(sql, [name, age]) as Array<Owner>;
+    return rows[0];
   }
 
 }
